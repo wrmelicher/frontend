4e5a242fe09f8962dc5a71054260e1325af969a4
Billy Melicher <wrm2ja@virginia.edu>
2012-05-28 12:26:12 -0400

    Initial source

--- src/frontend/ProgramTree.java
+++ src/frontend/ProgramTree.java
@@ -1,10 +1,39 @@
 package frontend;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.io.PrintStream;
+
 public class ProgramTree {
-  
+
+  private List<Statement> statements;
+  private List<Statement> inputs;
+  private List<Statement> outputs;
   public ProgramTree(){
-
+    statements = new ArrayList<Statement>();
+    inputs = new ArrayList<Statement>();
+    outputs = new ArrayList<Statement>();
   }
-
   
+  public void compile( PrintStream ps ) throws CompileException{
+
+    // inputs and stuff
+    compile_list( ps, inputs );
+    compile_list( ps, statements );
+    compile_list( ps, outputs );
+    ps.close();
+  }
+  private void compile_list( PrintStream ps, List<Statement> l ) throws CompileException{
+    for( Statement s : l ){
+      s.compile( ps );
+    }
+  }
+  public void addStatement( Statement s ){
+    if( s instanceof DeclareOutput )
+      outputs.add( s );
+    else if( s instanceof DeclareInputStatement )
+      inputs.add( s );
+    else 
+      statements.add( s );
+  }
 }
--- src/frontend/Variable.java
+++ src/frontend/Variable.java
@@ -1,11 +1,97 @@
 package frontend;
 
+import java.util.Set;
+import java.util.HashSet;
+import java.io.PrintStream;
+
 public class Variable {
-  private Type type;
-  public Variable( Type atype ) {
-    type = atype;
+  private TypeData type;
+  private long id;
+  private String high_level_name;
+  private long cur_assignment;
+  private boolean is_input;
+  private static long id_counter = 0;
+
+  public Variable( TypeData atype ){
+    this( unused_name(), atype );
   }
+  
+  public Variable( String name, TypeData atype ) {
+    type = atype;
+    id = ++id_counter;
+    high_level_name = name;
+    cur_assignment = 0;
+    is_input = false;
+  }
+
+  public static String temp_var_name(){
+    return "temp_var_" + (++id_counter);
+  }
+  public static String unused_name(){
+    return "poop";
+  }
+  
   public Type getType() {
+    return getData().getType();
+  }
+
+  public TypeData getData() {
     return type;
+  }
+  public void setData( TypeData d ){
+    type = d;
+  }
+  
+  public long getId() {
+    return id;
+  }
+  public long getCurAssign() {
+    return cur_assignment;
+  }
+  
+  public boolean equals( Object obj ){
+    if( obj instanceof Variable ){
+      Variable v = (Variable) obj;
+      return v.getId() == getId();
+    }
+    return false;
+  }
+  public int hashCode(){
+    return (int) id;
+  }
+  public String debug_name(){
+    return high_level_name;
+  }
+  public String cur_name(){
+    String cur = high_level_name + "_" + id;
+    if( is_input && cur_assignment == 0 ){
+      return debug_name();
+    }
+    if( cur_assignment == 0 )
+      return cur;
+    else 
+      return cur + "_" + cur_assignment;
+  }
+  public String new_name(){
+    inc_name();
+    return cur_name();
+  }
+
+  public void inc_name(){
+    cur_assignment++;
+  }
+  public void mark_as_input(){
+    is_input = true;
+  }
+
+  public void compile_assignment( PrintStream os, Variable other, AssignmentExp owner ) throws CompileException {
+    os.println( new_name() + " set " + other.cur_name() );
+    setData( other.getData() );
+  }
+  public static Variable BooleanVariable( String name ){
+    return new Variable( name, BoolData.INSTANCE );
+  }
+  public static Variable IntVariable( String name, IntTypeData i ){
+    return new Variable( name, i );
   }
 }
