a1a10949baf0208f5f58f7853a580a0a739f18c1
Billy Melicher <wrm2ja@virginia.edu>
2012-09-03 12:46:22 -0400

    working aes, and debug statements

--- src/frontend/DebugStatement.java
+++ src/frontend/DebugStatement.java
@@ -10,7 +10,12 @@ public class DebugStatement extends Statement {
     if( ProgramTree.DEBUG >= 1 ){
       val.compile();
       AbstractVariable v = val.returnVar();
+      if( v.var().getType() == Type.ArrayType ){
+	ArrayVariable out_arr = (ArrayVariable) v.var();
+	out_arr.join_indices();
+      }
       ProgramTree.error.println( v.var().toString() );
+      ProgramTree.output.println(".output "+v.var().cur_name());
     }
   }
 }
